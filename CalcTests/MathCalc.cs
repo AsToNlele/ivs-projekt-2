f19a7bbe4c54ce331a25519e9108cfadffbb3f0c
Petr Hybl <hybljunior@gmail.com>
2021-04-13 18:57:08 +0200

    notimplemented library

--- CalcTests/MathCalc.cs
+++ CalcTests/MathCalc.cs
@@ -6,6 +6,41 @@ namespace CalcTests
 {
     class MathCalc
     {
+        public static double Add(string num1, string num2)
+        {
+            throw new NotImplementedException();
+        }
+        public static double Sub(string num1, string num2)
+        {
+            throw new NotImplementedException();
+        }
+        public static double Mul(string num1, string num2)
+        {
+            throw new NotImplementedException();
+        }
 
+        public static double Div(string num1, string num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Pow(string num, string exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Sqrt(string num, string exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Abs(string num)
+        {
+            throw new NotImplementedException();
+        }
+        public static double Fact(string num)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
